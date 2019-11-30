bca6c37d038a5f4d93686855f5831410044c2c1e
Peter Kingsbury <peter.kingsbury@gmail.com>
2019-11-30 07:27:10 -0500

    fix id assignment on import

--- src/Entity.js
+++ src/Entity.js
@@ -148,7 +148,7 @@ export default class Entity {
   }
 
   import(data) {
-    this._id = data.id;
+    this.id = data.id;
     if (!data.hasOwnProperty("_components")) return;
     const worldComponents = this._world.componentsManager.getComponents();
     for (const [key, value] of Object.entries(data._components)) {
